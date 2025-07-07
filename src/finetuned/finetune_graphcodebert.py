e2c9734ee6c52b15b7924a7e67aa6f9be46d1e3f
Daniel <danielsfranco18@gmail.com>
2025-07-07 17:56:01 +0100

    fix

--- src/finetuned/finetune_graphcodebert.py
+++ src/finetuned/finetune_graphcodebert.py
@@ -16,7 +16,7 @@ from code_clone_pkg.dataset import CodeCloneDataset
 MODEL_NAME = "microsoft/graphcodebert-base"
 OUTPUT_DIR = "results/graphcodebert_finetune"
 MAX_LENGTH = 512
-EPOCHS = 9
+EPOCHS = 15
 BATCH_SIZE = 2
 CLONE_DATASETS = ['python_cobol', 'java_fortran', 'js_pascal']
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
